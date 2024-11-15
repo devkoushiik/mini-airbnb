747187b2c51e3e67d4abc737e40953393b00021d
koushikxahmed <koushikxahmed@gmail.com>
2024-11-15 12:36:44 +0600

    logo and navsearch added

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,4 +1,15 @@
-const Logo = () => {
-  return <div>Logo</div>;
-};
+import Link from "next/link";
+import { LuTent } from "react-icons/lu";
+import { Button, ButtonProps } from "../ui/button";
+
+function Logo() {
+  return (
+    <Button {...({ size: "icon", asChild: true } as ButtonProps)}>
+      <Link href="/">
+        <LuTent className="w-6 h-6" />
+      </Link>
+    </Button>
+  );
+}
+
 export default Logo;
--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,4 +1,12 @@
-const NavSearch = () => {
-  return <div>NavSearch</div>;
-};
+import { Input } from "../ui/input";
+
+function NavSearch() {
+  return (
+    <Input
+      type="search"
+      placeholder="find a property..."
+      className="max-w-xs dark:bg-muted "
+    />
+  );
+}
 export default NavSearch;
