16516eaea7a1cf6cd100e3491060271297e90d21
koushikxahmed <koushikxahmed@gmail.com>
2024-11-16 16:16:04 +0600

    form container refactor and ZOD implemented

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,26 +1,22 @@
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-const createProfileAction = async (formData: FormData) => {
-  "use server";
-  const firstName = formData.get("firstName") as string;
-  console.log(firstName);
-};
+import FormInput from "@/components/form/FormInput";
+
+import FormContainer from "@/components/form/FormContainer";
+import { SubmitButton } from "@/components/form/Button";
+import { createProfileAction } from "@/utils/actions";
 
 function CreateProfile() {
   return (
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
-      <div className="border p-8 rounded-md max-w-lg">
-        <form action={createProfileAction}>
-          <div className="mb-2">
-            <Label htmlFor="firstName">First Name</Label>
-            <Input id="firstName" name="firstName" type="text" />
+      <div className="border p-8 rounded-md">
+        <FormContainer action={createProfileAction}>
+          <div className="grid gap-4 mt-4 ">
+            <FormInput type="text" name="firstName" label="First Name" />
+            <FormInput type="text" name="lastName" label="Last Name" />
+            <FormInput type="text" name="username" label="Username" />
           </div>
-          <Button type="submit" {...{ size: "lg" }}>
-            Create Profile
-          </Button>
-        </form>
+          <SubmitButton text="Create Profile" className="mt-8" />
+        </FormContainer>
       </div>
     </section>
   );
