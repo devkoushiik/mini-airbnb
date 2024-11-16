467bd96ff9af0795ba97351e647f5a72b7c02c60
koushikxahmed <koushikxahmed@gmail.com>
2024-11-16 17:58:42 +0600

    create user profile done

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -3,8 +3,12 @@ import FormInput from "@/components/form/FormInput";
 import FormContainer from "@/components/form/FormContainer";
 import { SubmitButton } from "@/components/form/Button";
 import { createProfileAction } from "@/utils/actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-function CreateProfile() {
+async function CreateProfile() {
+  const user = await currentUser();
+  if (user?.privateMetadata?.hasProfile) redirect("/");
   return (
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
