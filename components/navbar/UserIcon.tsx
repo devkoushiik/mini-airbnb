52c2e237634bc38f5c9ccf36ee12462eff098b12
koushikxahmed <koushikxahmed@gmail.com>
2024-11-16 12:00:34 +0600

    link drop down added

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,4 +1,6 @@
-const UserIcon = () => {
-  return <div>UserIcon</div>;
-};
+import { LuUser2 } from "react-icons/lu";
+
+function UserIcon() {
+  return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
+}
 export default UserIcon;
